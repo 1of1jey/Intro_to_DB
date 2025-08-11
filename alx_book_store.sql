6dd9e2516d392ba52eda847ae3e028b3f208c811
jeffrey <eshunjeffrey12@gmail.com>
2025-08-11 03:30:28 +0000

    update

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
+
+    -- Create Authors table
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL
+);
+
