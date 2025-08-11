b7908db30b2d0c2aa3973cafeddbee01c210964f
jeffrey <eshunjeffrey12@gmail.com>
2025-08-11 03:31:34 +0000

    update

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -7,3 +7,10 @@ CREATE TABLE Authors (
     author_name VARCHAR(215) NOT NULL
 );
 
+-- Create Customers table
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT
+);
