ebe6b5aac0a3d78e05b5fd1b374fa054ba05c8de
jeffrey <eshunjeffrey12@gmail.com>
2025-08-11 23:18:24 +0000

    update

--- MySQLServer.py
+++ MySQLServer.py
@@ -5,3 +5,11 @@ from mysql.connector import Error
 def create_database():
     connection = None
     cursor = None
+
+try:
+    # Establish connection to MySQL server
+    connection = mysql.connector.connect(
+        host='localhost',        # MySQL server host
+        user='root',            # MySQL username
+        password='password'     # MySQL password (change as needed)
+    )
