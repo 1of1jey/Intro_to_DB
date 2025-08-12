298279c7504e75c794d7a02c515df7bde5ebcdf7
jeffrey <eshunjeffrey12@gmail.com>
2025-08-12 18:31:02 +0000

    update

--- task_2.sql
+++ task_2.sql
@@ -4,3 +4,12 @@ CREATE TABLE IF NOT EXISTS authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS books (
+                                     book_id INT AUTO_INCREMENT PRIMARY KEY,
+                                     title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+    );
